a785ebaf0b0b1dc0c04c35a98e04263ea2112c5f
Moussa Ghaddar <78150453+moses0072@users.noreply.github.com>
2021-05-25 13:01:50 +0200

    Added new tests

---
+++ src/NumberOfEvents.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class NumberOfEvents extends Component {
+  state = {
+    numberOfEvents: 5,
+  }
+
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    this.props.updateEvents('', value);
+    this.setState({
+      numberOfEvents: value,
+    });
+  };
+
+  render() {
+    return (
+      <div className='NumberOfEvents'>
+        <label htmlFor='numberOfEvent'>Number of Events</label>
+        <input
+          type='number'
+          className='event-number-input'
+          placeholder='Enter Number of Events to display'
+          value={this.state.numberOfEvents}
+          onChange={this.handleInputChanged}
+        />
+      </div>
+    );
+  } 
+}
+
+export default NumberOfEvents;
