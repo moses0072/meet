2ea03e540010a6ea312c4ef711fd498716a99bc2
Moussa Ghaddar <78150453+moses0072@users.noreply.github.com>
2021-05-26 16:02:08 +0200

    fixed bugs in api.js

--- src/api.js
+++ src/api.js
@@ -49,14 +49,12 @@ export const getEvents = async () => {
     const result = await axios.get(url);
     if (result.data) {
       var locations = extractLocations(result.data.events);
-      localStorage.setItem("lastEvents", JSON.stringify(result.data));
-      localStorage.setItem("locations", JSON.stringify(locations));
+      localStorage.setItem('lastEvents', JSON.stringify(result.data));
+      localStorage.setItem('locations', JSON.stringify(locations));
     }
     NProgress.done();
     return result.data.events;
   }
-};
-
 };
 
 export const getAccessToken = () => {
